ed3fa2b82696a3c8563d6a299ab7acdc6ce0ace0
yiwangai1983 <858994555@qq.com>
2015-04-23 10:54:55 +0800

    增加一个测试prop和service层继承的基类

---
+++ src/com/cores/corePlatform/base/service/BaseServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cores.corePlatform.base.service;
+
+import com.cores.corePlatform.base.PlatformBaseClass;
+
+/**
+ * serviceimpl继承基类.所有service实现类必须继承此类
+ * Created by Administrator on 2015/4/23.
+ */
+public class BaseServiceImpl extends PlatformBaseClass {
+
+}
